9ca5090b129a0a898f8a70216cb46a1507ba40b0
James Caldon <james.caldon@outlook.com>
2021-05-21 11:41:54 +0800

    Update preprocessing, automate real data collection

--- generated_data/Import-WSLData.ps1
+++ generated_data/Import-WSLData.ps1
@@ -1,19 +1,21 @@
 $baseNames = (Get-ChildItem -Path "\\wsl$\Ubuntu\home\james\generators\").BaseName
+#TODO: Copy Text file with parameters information
 
-
+$disc_size = "small_medium_discs"
 foreach ($baseName in $baseNames) {
     $dest = "C:\Users\James_Dev_Account\OneDrive - The University of Western Australia\Documents\Honours - Galaxy Classification\Galaxy-Classification-Research-Project\generated_data"
     if ($baseName.Contains("disc_1"))
     {
-        $dest = "$dest\disc"
+        $dest = "$dest\disc\$disc_size"
     } else 
     {
-        $dest = "$dest\no_disc"
+        $dest = "$dest\no_disc\$disc_size"
     }
     $directory = New-Item -Name $baseName $dest -ItemType directory -Force
     
     $item = Copy-Item "\\wsl$\Ubuntu\home\james\generators\$baseName\work.dir\*" $directory -PassThru
-    #Write-Host $item
+    Write-Host $item
+    Write-Host $directory
     tar -xf $item -C $directory
     Move-Item "$directory\m1.dir\*" $directory -Force
     Remove-Item "$directory\m1.dir" -Force
